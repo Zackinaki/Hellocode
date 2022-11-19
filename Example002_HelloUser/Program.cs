9ab28ba76988c345980cbacc04ab5424fc3b318d
Zack <exp.bur@gmail.com>
2022-11-19 19:39:30 +0800

    Add example002_hello

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example002_HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vvtdite vashe ima ");
+            string username = Console.ReadLine();
+            Console.Write("Hello, ");
+            Console.Write(username);
+        }
+    }
+}
