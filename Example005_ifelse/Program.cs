64c1178fafcb49c162ba07547045a296a1f82998
Zack <exp.bur@gmail.com>
2022-11-19 20:34:47 +0800

    Add example005

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example005_ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vvedite ima usera: ");
+            string username = Console.ReadLine();
+
+            if(username.ToLower() == "masha")
+            {
+                Console.WriteLine("Ura!!!, Masha");
+            }
+            else
+            {
+                Console.Write("Hi, ");
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
