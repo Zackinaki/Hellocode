61bf8fe66cc6412576f62dc16af5e72b5bdf4110
Zack <exp.bur@gmail.com>
2022-11-19 20:08:51 +0800

    Add example003

---
+++ Example004_div/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Example004_div
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = 12;
+            double numberB = 5;
+            Console.WriteLine(numberA / numberB);
+        }
+    }
+}
