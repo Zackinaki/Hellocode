abeb45b882a8a8a68bddfcfc83976ccbda27fa4a
Zack <exp.bur@gmail.com>
2022-11-19 20:02:05 +0800

    Add example003

---
+++ Example003_Some/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example003_Some
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 31;
+            int numberB = 5;
+            int result = numberA + numberB;
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
